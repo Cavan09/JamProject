128ec7d5f290ebf9992c7de42c49b76c84d6cc4f
Greginald <greg.l.coghill@gmail.com>
2017-01-21 16:12:39 -0500

    Added my shit
    Dope ass assets i worked on alone.

---
+++ GameJam/Assets/Standard Assets/Greg/Scripts/Exit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+
+    public GameObject m_Destination;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player1"))
+        {
+            other.transform.position = m_Destination.transform.position;
+        }
+    }
+}
