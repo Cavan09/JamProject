b741bee20df7cf850059ad0954a4f9ffb293ebd7
Cavan09 <cavan09@gmail.com>
2017-01-21 22:43:16 -0500

    Commit for pull request

--- GameJam/Assets/Standard Assets/Greg/Scripts/TestWave.cs
+++ GameJam/Assets/Standard Assets/Greg/Scripts/TestWave.cs
@@ -5,28 +5,28 @@ using UnityEngine;
 public class TestWave : MonoBehaviour
 {
 
-    public CameraShake m_CameraShake;
+ //   public CameraShake m_CameraShake;
 
-	// Use this for initialization
-	void Start ()
-    {
+	//// Use this for initialization
+	//void Start ()
+ //   {
 		
-	}
+	//}
 	
-	// Update is called once per frame
-	void Update ()
-    {
-        transform.Translate(0.1f, 0.0f, 0.0f);
-	}
+	//// Update is called once per frame
+	//void Update ()
+ //   {
+ //       transform.Translate(0.1f, 0.0f, 0.0f);
+	//}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        //Do the animation thing with waves
-        //Do the harlem shake
-      //  if (other.CompareTag("Wall"))
-       // {
-            m_CameraShake.shakeDuration = 1.0f;
-       // }
-        transform.Translate(-30.0f, 0.0f, 0.0f);
-    }
+ //   private void OnTriggerEnter(Collider other)
+ //   {
+ //       //Do the animation thing with waves
+ //       //Do the harlem shake
+ //     //  if (other.CompareTag("Wall"))
+ //      // {
+ //           m_CameraShake.shakeDuration = 1.0f;
+ //      // }
+ //       transform.Translate(-30.0f, 0.0f, 0.0f);
+ //   }
 }
